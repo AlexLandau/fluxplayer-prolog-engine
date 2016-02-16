a11a702de03b1444aa0bd430f3aec1c4ddd25ed4
Alex <alandau@cs.stanford.edu>
2016-02-15 23:33:07 -0800

    Make shell script that works with perf framework

--- start_perf_test.sh
+++ start_perf_test.sh
@@ -2,9 +2,8 @@
 
 ECLIPSE="/home/alex/prologEclipse/bin/x86_64_linux/eclipse -g 512M"
 
-GAME_FILE="/home/alex/code/fluxplayer-prolog-engine/sample_game.kif"
-OUTPUT_FILE="/home/alex/code/fluxplayer-prolog-engine/output.out"
-SECONDS_TO_RUN=1
+GAME_FILE=$1
+OUTPUT_FILE=$2
+SECONDS_TO_RUN=$3
 
 ${ECLIPSE} -b "bin/main_perf" -e "main_perf:run_perf_test(\"${GAME_FILE}\", \"${OUTPUT_FILE}\", ${SECONDS_TO_RUN})"
-
---
+++ start_sample_perf_test.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+GAME_FILE="/home/alex/code/fluxplayer-prolog-engine/sample_game.kif"
+OUTPUT_FILE="/home/alex/code/fluxplayer-prolog-engine/output.out"
+SECONDS_TO_RUN=2
+
+./start_perf_test.sh $GAME_FILE $OUTPUT_FILE $SECONDS_TO_RUN
