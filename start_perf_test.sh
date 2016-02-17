a6dfe80196abf4c806848da9e61f4d3299622682
Alex <alandau@cs.stanford.edu>
2016-02-16 23:09:03 -0800

    Make the scripts a bit more general.
    Expect an ECLIPSE environment variable to be set to define where
    Eclipse Prolog is installed.

--- start_perf_test.sh
+++ start_perf_test.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-ECLIPSE="/home/alex/prologEclipse/bin/x86_64_linux/eclipse -g 512M"
+# The perf framework should set the ECLIPSE environment variable
+# Try "eclipse" if ECLIPSE is not defined
+ECLIPSE="${ECLIPSE:-eclipse}"
 
 GAME_FILE=$1
 OUTPUT_FILE=$2
 SECONDS_TO_RUN=$3
 
-${ECLIPSE} -b "bin/main_perf" -e "main_perf:run_perf_test(\"${GAME_FILE}\", \"${OUTPUT_FILE}\", ${SECONDS_TO_RUN})"
+${ECLIPSE} -g 512M -b "bin/main_perf" -e "main_perf:run_perf_test(\"${GAME_FILE}\", \"${OUTPUT_FILE}\", ${SECONDS_TO_RUN})"
